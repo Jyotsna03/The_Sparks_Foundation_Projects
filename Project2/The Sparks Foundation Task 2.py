6bff4772147a20719349e5715066d0ae7e2ea9b9
Jyotsna Bhatia <69782687+Jyotsna03@users.noreply.github.com>
2024-07-26 08:11:41 +0530

    Update The Sparks Foundation Task 2.py

--- Project2/The Sparks Foundation Task 2.py
+++ Project2/The Sparks Foundation Task 2.py
@@ -3,7 +3,7 @@
 
 # # The Sparks Foundation
 # ### GRIP Septemer 2021 Batch 
-# ### Author: Teghpreet Singh Mago
+# ### Author: Jyotsna Bhatia
 # ### Task 2 : Color Identification in Images
 
 # ### 1. Problem defintion
